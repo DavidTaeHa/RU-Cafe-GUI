473c5cae0e0b4078dc31e09a537c5e595e6208a8
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-04-10 22:00:03 -0400

    Update

--- app/src/main/java/com/example/rucafe/MainActivity.java
+++ app/src/main/java/com/example/rucafe/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.rucafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void openDonutMenu(View view) {
+    }
+
+    public void openCoffeeMenu(View view) {
+    }
+
+    public void openCart(View view) {
+    }
+
+    public void openOrders(View view) {
+    }
+
 }
