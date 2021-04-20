ae6c636faf6c485342fb7eb2b05dec101f1910b4
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-04-20 18:29:26 -0400

    Update Database.java

--- app/src/main/java/com/example/rucafe/Database.java
+++ app/src/main/java/com/example/rucafe/Database.java
@@ -3,8 +3,14 @@ package com.example.rucafe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Database extends AppCompatActivity {
+
+    ListView orderList;
+    StoreOrders storeOrders;
+    ArrayAdapter<Order> list;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +18,9 @@ public class Database extends AppCompatActivity {
         setContentView(R.layout.activity_database);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         setTitle(R.string.database_menu);
+        orderList = findViewById(R.id.pastOrders);
+        storeOrders = MainActivity.getStoreOrder();
+        list = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, storeOrders.getStoreOrders());
+        orderList.setAdapter(list);
     }
 }
