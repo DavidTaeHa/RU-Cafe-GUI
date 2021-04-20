19de1eb9df84ec700699b8ccd622a6708406bda3
Gandhi115 <61490541+Gandhi115@users.noreply.github.com>
2021-04-20 18:37:11 -0400

    Update Database.java

--- app/src/main/java/com/example/rucafe/Database.java
+++ app/src/main/java/com/example/rucafe/Database.java
@@ -1,12 +1,18 @@
 package com.example.rucafe;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class Database extends AppCompatActivity {
+import java.text.NumberFormat;
+
+public class Database extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     ListView orderList;
     StoreOrders storeOrders;
@@ -22,5 +28,24 @@ public class Database extends AppCompatActivity {
         storeOrders = MainActivity.getStoreOrder();
         list = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, storeOrders.getStoreOrders());
         orderList.setAdapter(list);
+        orderList.setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        AlertDialog.Builder warning = new AlertDialog.Builder(Database.this);
+        warning.setTitle("Cancel Order");
+        warning.setMessage("Are you sure you want to cancel this order?");
+        Order item = storeOrders.getStoreOrders().get(position);
+        warning.setNegativeButton("No", null);
+        warning.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                storeOrders.remove(item);
+                list.notifyDataSetChanged();
+            }
+        });
+        warning.show();
     }
 }
